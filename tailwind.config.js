8aadef1414474a262431dc5b1ee8ab0e1a86c041
Pieter Verweij <pieterverweij2006@outlook.com>
2023-04-18 11:22:29 +0200

    fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "misty-rose": "#FDE8E9",
+        "fairy-tale": "#E3BAC6",
+        "lilac": "#BC9EC1",
+        "paynes-gray": "#596475",
+        "raisin-black": "#1F2232",
+      },
+    },
   },
   plugins: [],
 }
