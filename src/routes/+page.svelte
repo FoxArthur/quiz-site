68ff8d34f1261270716ebed1fbc685a16c261aef
Arthur Vos <arthurvos06@gmail.com>
2023-05-08 13:22:40 +0200

    sick animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,14 +8,13 @@
   <body>
     <div class="px-6 py-20 text-center text-paynes-gray bg-fairy-tale">
       <h1 class="mb-6 text-5xl font-bold">QuizzyLizzy</h1>
-      <h3 class="mb-8 text-3xl font-bold">Door Arthur en Pieter</h3>
+      <h3 class="mb-8 text-3xl font-bold ">Door Arthur en Pieter</h3>
       <a
         class="inline-block rounded bg-paynes-gray primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-paynes-gray transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
         data-te-ripple-init
         data-te-ripple-color="light"
         href="quiz"
-        role="button">Start de quiz</a
-      >
+        role="button">Start de quiz</a>
     </div>
 
     <div class="grid grid-cols-2 gap-4 text-center mt-4">
@@ -26,7 +25,7 @@
               Over de quiz:
             </h3>
 
-            <p class="mt-2 text-sm text-gray-500">
+            <p class="mt-2 text-sm text-gray-500 animate-spin">
               Wij hebben deze quiz gemaakt om op deze manier onze frontend skill
               bij te scherpen. <br />
               Deze quiz heeft voornamelijk het doel om ons te trainen in het gebruiken
