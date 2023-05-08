d7bbb29280fc7d407ac8c7d6294bdf6d0e4cd373
Pieter Verweij <pieterverweij2006@outlook.com>
2023-05-08 12:52:48 +0200

    changed the links to the right page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,65 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-</head>
-<body>
-   
-    <div
-    class="px-6 py-20 text-center text-paynes-gray bg-fairy-tale ">
-    <h1 class="mb-6 text-5xl font-bold">QuizzyLizzy</h1>
-    <h3 class="mb-8 text-3xl font-bold">Door Arthur en Pieter</h3>
-    <a
-      class="inline-block rounded bg-paynes-gray primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-paynes-gray transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-      data-te-ripple-init
-      data-te-ripple-color="light"
-      href="#!"
-      role="button"
-      >Start de quiz</a
-    >
-  </div>
+  </head>
+  <body>
+    <div class="px-6 py-20 text-center text-paynes-gray bg-fairy-tale">
+      <h1 class="mb-6 text-5xl font-bold">QuizzyLizzy</h1>
+      <h3 class="mb-8 text-3xl font-bold">Door Arthur en Pieter</h3>
+      <a
+        class="inline-block rounded bg-paynes-gray primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-paynes-gray transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+        data-te-ripple-init
+        data-te-ripple-color="light"
+        href="quiz"
+        role="button">Start de quiz</a
+      >
+    </div>
 
-  
+    <div class="grid grid-cols-2 gap-4 text-center mt-4">
+      <div>
+        <h1 class="block rounded-xl bg-white p-4 sm:p-6 lg:p-8">
+          <div class="">
+            <h3 class="text-lg font-bold sm:text-xl text-paynes-gray">
+              Over de quiz:
+            </h3>
 
-  <div class="grid grid-cols-2 gap-4 text-center mt-4">
+            <p class="mt-2 text-sm text-gray-500">
+              Wij hebben deze quiz gemaakt om op deze manier onze frontend skill
+              bij te scherpen. <br />
+              Deze quiz heeft voornamelijk het doel om ons te trainen in het gebruiken
+              van CSS animaties.
+            </p>
+          </div>
+        </h1>
+      </div>
 
-    <div>
-      <h1 class="block rounded-xl bg-white p-4 sm:p-6 lg:p-8">
-        <div class="">
-          <h3 class="text-lg font-bold sm:text-xl text-paynes-gray">
-            Over de quiz:
-          </h3>
-    
-          <p class="mt-2 text-sm text-gray-500">
-            Wij hebben deze quiz gemaakt om op deze manier onze frontend skill bij te scherpen. <br>
-             Deze quiz heeft voornamelijk het doel om ons te trainen in het gebruiken van CSS animaties.
-          </p>
-        </div>
-    </h1>
+      <div>
+        <h1 class="block rounded-xl bg-white p-4 sm:p-6 lg:p-8">
+          <div class="">
+            <h3 class="text-lg font-bold sm:text-xl text-paynes-gray">
+              Over ons:
+            </h3>
+
+            <p class="mt-2 text-sm text-gray-500">
+              Wij zijn Pieter en Arthur, wij zijn studenten aan het Hoornbeeck
+              college in Amersfoort <br />
+              Wij zitten beiden in het tweede jaar van onze opldeiding, Software
+              Development.
+            </p>
+          </div>
+        </h1>
+      </div>
     </div>
 
     <div>
-      <h1 class="block rounded-xl bg-white p-4 sm:p-6 lg:p-8">
-        <div class="">
-          <h3 class="text-lg font-bold sm:text-xl text-paynes-gray">
-            Over ons:
-          </h3>
-    
-          <p class="mt-2 text-sm text-gray-500">
-            Wij zijn Pieter en Arthur, wij zijn studenten aan het Hoornbeeck college in Amersfoort <br>
-            Wij zitten beiden in het tweede jaar van onze opldeiding, Software Development.
-          </p>
-        </div>
-    </h1>
+      <script
+        data-name="BMC-Widget"
+        data-cfasync="false"
+        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+        data-id="FoxArthur"
+        data-description="Support me on Buy me a coffee!"
+        data-message=""
+        data-color="#e3bac6"
+        data-position="Right"
+        data-x_margin="18"
+        data-y_margin="18"
+      ></script>
     </div>
-  
-</div>
-
-  <div>
-    <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="FoxArthur" data-description="Support me on Buy me a coffee!" data-message="" data-color="#e3bac6" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
-  </div>
-
-</body>
+  </body>
 </html>
